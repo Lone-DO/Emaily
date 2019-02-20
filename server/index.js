364e4c75ad871e405e9b615a51112b21f02008c2
lone-DO <lotussythe@gmail.com>
2019-02-20 14:35:38 -0500

    Staging Google Auth

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = requrie('./config/keys');
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "There" });
-});
+passport.use(new GoogleStrategy(
+  clientID: keys.googleClient,
+  clientSecret: keys.googleSecret,
+  callbackURL: '/auth/google/callback'
+));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);

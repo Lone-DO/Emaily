a7ea3ef607baa42fb4e06cd2d1bb7eb837215259
lone-DO <lotussythe@gmail.com>
2019-02-23 03:02:56 -0500

    Enabling Cookies & Handling Asynchronous errors

--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
 
-mongoose.connect(keys.mongoURI);
+// Avoiding Mongo 4.0 deprecation error by enabling option, useNewUrlParser
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express();
+
+app.use(
+  cookieSession({
+    // 30 days  * 24hrs * 60min * 60sec * 1000 milisecond => converting to milliseconds
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authrouts")(app);
 

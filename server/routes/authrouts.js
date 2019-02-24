ee41ddadcaeaa76b9442b8547adc9ff22a91eb1d
lone-DO <lotussythe@gmail.com>
2019-02-24 18:26:28 -0500

    Enabling Github Authentication

--- server/routes/authrouts.js
+++ server/routes/authrouts.js
@@ -1,6 +1,7 @@
 const passport = require("passport");
 
 module.exports = app => {
+  //Google Authentication
   app.get(
     "/auth/google",
     passport.authenticate("google", {
@@ -9,6 +10,20 @@ module.exports = app => {
   );
 
   app.get("/auth/google/callback", passport.authenticate("google"));
+  //end googleAuth
+
+  //Github Authentication
+  app.get("/auth/github", passport.authenticate("github"));
+
+  app.get(
+    "/auth/github/callback",
+    passport.authenticate("github", { failureRedirect: "/login" }),
+    (req, res) => {
+      // Successful authentication, redirect home.
+      res.redirect("/");
+    }
+  );
+  //end githubAuth
 
   app.get("/api/current_user", (req, res) => {
     res.send(req.user);
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,5 +1,6 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const GitHubStrategy = require("passport-github").Strategy;
 const mongoose = require("mongoose");
 const keys = require("../config/keys");
 
@@ -30,6 +31,27 @@ passport.use(
           done(null, existingUser);
         } else {
           new User({ googleID: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
+    }
+  )
+);
+
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: keys.localGClientID,
+      clientSecret: keys.localGClientSecret,
+      callbackURL: "/auth/github/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ githubID: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ githubID: profile.id })
             .save()
             .then(user => done(null, user));
         }

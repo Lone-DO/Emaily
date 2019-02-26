03ed65c95cade6da6d20af3dcf5fb7d8fd6cfbbf
lone-DO <lotussythe@gmail.com>
2019-02-26 00:51:01 -0500

    Enabling Logout route

--- server/routes/authrouts.js
+++ server/routes/authrouts.js
@@ -28,6 +28,11 @@ module.exports = app => {
   );
   //end githubAuth
 
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  })
+
   app.get("/api/current_user", (req, res) => {
     res.send(req.user);
   });

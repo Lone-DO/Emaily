8956e85f6ad8b032ffb33d93e2c00ca847a41b85
lone-DO <lotussythe@gmail.com>
2019-02-28 01:46:52 -0500

    Refactoring with ES2017

--- server/services/passport.js
+++ server/services/passport.js
@@ -25,17 +25,15 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleID: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // we already have a record with the provided user information
-          done(null, existingUser);
-        } else {
-          new User({ googleID: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleID: profile.id });
+      if (existingUser) {
+        // we already have a record with the provided user information
+        done(null, existingUser);
+      } else {
+        const user = await new User({ googleID: profile.id }).save();
+        done(null, user);
+      }
     }
   )
 );
@@ -48,15 +46,13 @@ passport.use(
       callbackURL: "/auth/github/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ githubID: profile.id }).then(existingUser => {
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ githubID: profile.id }).then(existingUser => {
         if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({ githubID: profile.id })
-            .save()
-            .then(user => done(null, user));
+          return done(null, existingUser);
         }
+        const user = new User({ githubID: profile.id }).save();
+        done(null, user);
       });
     }
   )

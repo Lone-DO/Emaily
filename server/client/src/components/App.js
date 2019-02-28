4134476c16dce8a4004f09f4e323361aef0c99a9
lone-DO <lotussythe@gmail.com>
2019-02-28 02:36:16 -0500

    React Component Placeholders

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2></h2>
+const Dashboard = () => <h2></h2>
+const SurveyNew = () => <h2></h2>
+const Landing = () => <h2></h2>
 
 const App = () => {
     return (

1574e162559331d4cb9b97098199ff8d0db08896
lone-DO <lotussythe@gmail.com>
2019-03-04 03:05:06 -0500

    PATCH: Fixed error port 3000 not exchanging data with APIs

--- server/client/src/setupProxy.js
+++ server/client/src/setupProxy.js
@@ -2,6 +2,7 @@ const proxy = require("http-proxy-middleware");
 const local = "http://localhost:5000";
 
 module.exports = function(app) {
-  app.use(proxy("/auth/*", { target: local }));
-  app.use(proxy("/api/*", { target: local }));
+  app.use(proxy("/auth/*", { target: "http://localhost:5000" }));
+  app.use(proxy("/api/*", { target: "http://localhost:5000" }));
+  app.use(proxy("/auth/google/callback", { target: "http://localhost:5000" }));
 };

9213fa16e6bbf60111b826ad1a6cd7d43f3e403a
lone-DO <lotussythe@gmail.com>
2019-03-04 03:07:51 -0500

    Update HEADER content depending if user is signed in or not

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { FETCH_USER } from "./types";
 
 export const fetchUser = () => async dispatch => {
-  const res = axios.get("/api/current_user");
-  dispatch({ type: FETCH_USER, payload: res });
+  const res = await axios.get("/api/current_user");
+  // refer Payload to take only focus on returned data of the response
+  dispatch({ type: FETCH_USER, payload: res.data });
 };

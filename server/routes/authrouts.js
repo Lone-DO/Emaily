f6a276049600b582a42c0f44bef2e7675b473ad2
lone-DO <lotussythe@gmail.com>
2019-03-04 03:21:18 -0500

    Setup redirecting user

--- server/routes/authrouts.js
+++ server/routes/authrouts.js
@@ -5,11 +5,22 @@ module.exports = app => {
   app.get(
     "/auth/google",
     passport.authenticate("google", {
-      scope: ["profile", "email"]
+      // Options below are required when making request to google API
+      // Scope, inquires the profile ID & User's Email
+      // Prompt, inquires the user to select which account to utilize
+      scope: ["profile", "email"],
+      prompt: "select_account"
     })
   );
 
-  app.get("/auth/google/callback", passport.authenticate("google"));
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google", { failureRedirect: "/auth/google" }),
+    (req, res) => {
+      // Successful authentication, redirect to Dashboard.
+      res.redirect("/surveys");
+    }
+  );
   //end googleAuth
 
   //_______Github Authentication_______
@@ -20,7 +31,7 @@ module.exports = app => {
 
   app.get(
     "/auth/github/callback",
-    passport.authenticate("github", { failureRedirect: "/login" }),
+    passport.authenticate("github", { failureRedirect: "/auth/github" }),
     (req, res) => {
       // Successful authentication, redirect home.
       res.redirect("/");
@@ -29,11 +40,13 @@ module.exports = app => {
   //end githubAuth
 
   app.get("/api/logout", (req, res) => {
+    // send request to api to clear the cache
     req.logout();
-    res.send(req.user);
+    res.redirect("/");
   });
 
   app.get("/api/current_user", (req, res) => {
+    // See logged in user model within the cache
     res.send(req.user);
   });
 };

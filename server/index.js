b712bfb4c29aae4c60899d48aa678091332a9734
lone-DO <lotussythe@gmail.com>
2019-03-07 23:50:46 -0500

    Setup Production Enviroment

--- server/index.js
+++ server/index.js
@@ -27,5 +27,18 @@ app.use(passport.session());
 require("./routes/authrouts")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  //express will serve up production assets
+  //like our main.js file, or main.css file!
+  app.use(express.static("client/build/"));
+
+  //express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);

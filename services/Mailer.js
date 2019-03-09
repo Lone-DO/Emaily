ce2a38f361298018e7456914777096c989387c77
lone-DO <lotussythe@gmail.com>
2019-03-09 01:41:23 -0500

    Mailer preperation

--- services/Mailer.js
+++ services/Mailer.js
@@ -3,7 +3,24 @@ const helper = sendgrid.mail;
 const keys = require("../config/keys");
 
 class Mailer extends helper.Mail {
-  //
+  constructor({ subject, recipients }, content) {
+    super();
+
+    this.from_email = new helper.Email("no-reply@emaily.com");
+    this.subject = subject;
+    this.body = new helper.Content("text/html", content);
+    this.recipients = this.formatAddresses(recipients);
+
+    // Bind target body to content
+    this.addContent(this.body);
+  }
+
+  // Seperate email properties from array
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    });
+  }
 }
 
 module.exports = Mailer;

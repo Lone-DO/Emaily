3e788f2acc79b35c9bafafc4a760e8dde6793357
lone-DO <lotussythe@gmail.com>
2019-03-09 01:51:47 -0500

    Mailer: Finished Setup

--- services/Mailer.js
+++ services/Mailer.js
@@ -6,6 +6,7 @@ class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super();
 
+    this.sgApi = sendgrid(keys.sendgridKey);
     this.from_email = new helper.Email("no-reply@emaily.com");
     this.subject = subject;
     this.body = new helper.Content("text/html", content);
@@ -13,7 +14,9 @@ class Mailer extends helper.Mail {
 
     // Bind target body to content
     this.addContent(this.body);
+
     this.addClickTracking();
+    this.addRecipients();
   }
 
   // Seperate email properties from array
@@ -30,6 +33,26 @@ class Mailer extends helper.Mail {
 
     trackingSettings.setClickTracking(clickTracking);
     this.addTrackingSettings(trackingSettings);
+  }
+
+  addRecipients() {
+    const personalize = new helper.Personalization();
+
+    this.recipients.forEach(recipient => {
+      personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
+  }
+
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: "POST",
+      path: "/v3/mail/send",
+      body: this.toJSON()
+    });
+
+    const response = this.sgApi.API(request);
+    return response;
   }
 }
 

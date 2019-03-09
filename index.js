071efa8f34a3a141f60bae275b089fdc3efb2199
lone-DO <lotussythe@gmail.com>
2019-03-08 23:43:31 -0500

    Setup Models with Sub Document Schema(s) for surveys

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const passport = require("passport");
 const keys = require("./config/keys");
 const bodyParser = require("body-parser");
 require("./models/User");
+require("./models/Survey");
 require("./services/passport");
 
 // Avoiding Mongo 4.0 deprecation error by enabling option, useNewUrlParser

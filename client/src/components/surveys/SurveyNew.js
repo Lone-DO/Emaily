8c97e74e259fab0c45017d1ab732094766a73f5e
lone-DO <lotussythe@gmail.com>
2019-03-12 03:11:39 -0400

    Initial setup for redux-form (boilerplate)

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class SurveyNew extends Component {
+  render() {
+    return (
+      // Component Boiler plate setup
+      <div>SurveyNew</div>
+    );
+  }
+}
+
+export default SurveyNew;

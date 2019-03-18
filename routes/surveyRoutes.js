190e87f5c341ed992a21d01b24cd841ec9a100e8
lone-DO <lotussythe@gmail.com>
2019-03-18 04:45:53 -0400

    setting up webhooks/ localtunnel

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -34,6 +34,11 @@ module.exports = app => {
     }
   });
 
+  app.post("/api/surveys/webhooks", (req, res) => {
+    console.log(req.body);
+    res.send({});
+  });
+
   app.get("/api/surveys/thanks", (req, res) => {
     res.send("Thanks for your feedback!");
   });

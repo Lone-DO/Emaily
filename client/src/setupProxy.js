a503de6a3d76cc01d3ed53e4b71b94056d6636a6
lone-DO <lotussythe@gmail.com>
2019-03-20 02:17:18 -0400

    Filtering responses from Sendgrid, edit redirects

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -5,6 +5,6 @@ module.exports = function(app) {
   app.use(proxy("/auth/*", { target: "http://localhost:5000" }));
   app.use(proxy("/auth/google/callback", { target: "http://localhost:5000" }));
   app.use(proxy("/api/*", { target: "http://localhost:5000" }));
-  app.use(proxy("/api/surveys", { target: "http://localhost:5000" }));
-  app.use(proxy("/api/surveys/thanks", { target: "http://localhost:5000" }));
+  app.use(proxy("/api/surveys/*", { target: "http://localhost:5000" }));
+  app.use(proxy("/api/surveys/*/*", { target: "http://localhost:5000" }));
 };

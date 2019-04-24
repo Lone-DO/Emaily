0fcf75fba6ff8c22a28ae8a6ff8202902f267937
lone-DO <lotussythe@gmail.com>
2019-04-24 16:32:19 +0200

    Update mongo with responses, lowercase all emails

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -17,7 +17,9 @@ module.exports = app => {
       title,
       subject,
       body,
-      recipients: recipients.split(",").map(email => ({ email: email.trim() })),
+      recipients: recipients
+        .split(",")
+        .map(email => ({ email: email.toLowerCase().trim() })),
       _user: req.user.id,
       dateSent: Date.now()
     });
@@ -39,7 +41,7 @@ module.exports = app => {
 
   app.post("/api/surveys/webhooks", (req, res) => {
     const p = Path.createPath("/api/surveys/:surveyId/:choice");
-    const events = _.chain(req.body)
+    _.chain(req.body)
       .map(({ email, url }) => {
         const match = p.test(new URL(url).pathname);
         if (match) {
@@ -48,13 +50,26 @@ module.exports = app => {
       })
       .compact()
       .uniqBy("email", "surveyId")
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email: email, responded: false }
+            }
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { "recipients.$.responded": true },
+            lastResponded: new Date()
+          }
+        ).exec();
+      })
       .value();
-    console.log(events);
-
     res.send({});
   });
 
-  app.get("/api/surveys/thanks", (req, res) => {
+  app.get("/api/surveys/:surveyId/:choice", (req, res) => {
     res.send("Thanks for your feedback!");
   });
 };
